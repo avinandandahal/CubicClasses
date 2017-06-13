e4a7f3c50fa98ed50daf246b9d5dbd7129ff162c
avi <avidahal02@gmail.com>
2017-06-13 11:41:21 -0500

    edited by Avi

--- CubicClasses/Avi/UI_Class/NodeExpressAssignment/ExpressApp.js
+++ CubicClasses/Avi/UI_Class/NodeExpressAssignment/ExpressApp.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var app = express();
-
-app.get('/',function(request, response){
-    response.send("Hello World");
-});
-
+var authmode = require('./AuthModule');
+var url = require('url');
+var bodyParser = require('body-parser');
 var server = app.listen(3000);
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.post('',function(request, response){
+    // response.send("Username Password Match");
+    var username = request.body.username;
+    var password = request.body.password;
+
+    console.log(username);
+    console.log(password);
+
+    var userAuthenticate = authmode.AuthUser(username, password);
+    console.log(userAuthenticate);
+
+    if( userAuthenticate == true){
+        response.sendFile("E:\\Cubic\\UICLASS_Practice\\ExpressAndRestAPI\\CubicHome.html");
+    }
+    else
+        response.sendFile("E:\\Cubic\\UICLASS_Practice\\ExpressAndRestAPI\\index-accessdenied.html");
+});
